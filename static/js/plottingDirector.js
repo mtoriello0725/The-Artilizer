13aa9d6073c9668407cb358f62829e07feeff1cd
mtoriello0725 <34101012+mtoriello0725@users.noreply.github.com>
2018-12-06 12:00:42 -0500

    Created plotting director js file to display charts on display page.

---
+++ static/js/plottingDirector.js
@@ -0,0 +1,15 @@
+function plotManager(artistName) {
+	/* 
+	This function will trigger all of the plotting functions and return
+	outputs to the proper html tags. Will need to make calls for:
+
+	- Mean values of each attribute (in a melt dataframe.)
+
+	- all album names (possibly album images) to visualize attributes by album
+
+	- attributes by album (album names above will be trigger for album charts)
+
+	- top popular songs 
+
+	*/
+}
