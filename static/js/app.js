705afd5d5dc937f53a6b5b9309935e9e95c792a1
mtoriello0725 <34101012+mtoriello0725@users.noreply.github.com>
2018-12-10 15:24:13 -0500

    removed entire script for d3 selecting, and instead redirect from app.py

--- static/js/app.js
+++ static/js/app.js
@@ -1,44 +1,10 @@
 
-function submitQuery(input_id) {
-	/* send input value to route of python script */
+// Use the window parameters to tell javascript what the queried artist was.
+if (window.location.pathname == "/artist/display") {
 
-	// Assign input variable to text in appropriate search bar
-	var inputArtist = d3.select(input_id);
+	var url = new URL(window.location.href);
+	searchArtist = url.searchParams.get("artist_name");
+	console.log(searchArtist);
 
-	// Pull artist name from search bar
-	var artistValue = inputArtist.property("value");
-
-	// Create route name to send to flask app. 
-	var query_url = "/api/artist/"+artistValue;
-
-	// Access route with artist name as the input. Return with "Success" or "Failed" response.
-	d3.json(query_url).then(function(response) {
-		console.log(response);
-	});
-
-	// If response "success", send to plotting functions with artist as input.
-	if (response == "Success") {
-		return plotManager(artistValue);
-	} else {
-		return response;
-	}
+	var test = d3.select("#testing").append("p").text("Does this testing text appear on display page");
 }
-
-// select the submit buttons
-var submit_nav = d3.select('#submit_nav');
-var submit_home = d3.select('#submit_home');
-
-// when a name appears in either search bar, run submitQuery function.
-submit_nav.on("click", function() {
-
-	d3.event.preventDefault();
-	submitQuery("#artist-input-nav");
-
-});
-
-submit_home.on("click", function() {
-
-	d3.event.preventDefault();
-	submitQuery("#artist-input-home");
-
-});
